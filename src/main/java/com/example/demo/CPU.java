a607a83125d46d22b53b64b6193dca17c5310511
hedong <755964539@qq.com>
2019-08-01 16:33:51 +0800

    ks sig fail for linux, err msg:Unable to load library 'core': libcore.so: cannot open shared object file: No such file or directory /usr/lib/libcore.so: wrong ELF class: ELFCLASS32 Can't obtain InputStream for linux-x86-64/libcore.so

--- src/main/java/com/example/demo/CPU.java
+++ src/main/java/com/example/demo/CPU.java
@@ -8,27 +8,6 @@ public interface CPU extends Library {
 
     CPU INSTANCE = (CPU) Native.loadLibrary("core", CPU.class);
 
-    String a(Object object, byte[] paramArrayOfByte, int paramInt);// Byte code:
-    //   0: ldc com/yxcorp/gifshow/util/CPU
-    //   2: monitorenter
-    //   3: aload_0
-    //   4: aload_1
-    //   5: iload_2
-    //   6: invokestatic getClock : (Landroid/content/Context;[BI)Ljava/lang/String;
-    //   9: astore_0
-    //   10: ldc com/yxcorp/gifshow/util/CPU
-    //   12: monitorexit
-    //   13: aload_0
-    //   14: areturn
-    //   15: astore_0
-    //   16: ldc com/yxcorp/gifshow/util/CPU
-    //   18: monitorexit
-    //   19: aload_0
-    //   20: athrow
-    // Exception table:
-    //   from	to	target	type
-    //   3	10	15	finally }
-
     String getClock(Object object, byte[] paramArrayOfByte, int paramInt);
 
     String getMagic(Object object, int paramInt);
--- src/main/java/com/example/demo/KSController.java
+++ src/main/java/com/example/demo/KSController.java
@@ -1,18 +1,21 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
-@RestController
+@Controller
 @RequestMapping("/ks")
 public class KSController {
     static final String KSAPI = "http://api.ksapisrv.com/rest/n/photo/info2?kpf=IPHONE&net=%E4%B8%AD%E5%9B%BD%E7%A7%BB%E5%8A%A8_5&appver=6.6.2.1004&kpn=KUAISHOU&mod=iPhone8%2C1&c=a&ud=214451601&did_gt=1551354412209&ver=6.6&sys=ios12.3.1&did=39896A34-183F-44F6-A9D3-29553C501FE3&isp=CMCC";
 
     @GetMapping("/kstest")
+    @ResponseBody
     public String ksSig() {
         Map<String, String> stringStringMap = KuaiShouDecodeMain.urlDecode(KSAPI);
         System.out.println(stringStringMap);
