721950cb232487f949383c8f21ab7eec2455e803
hedong <755964539@qq.com>
2019-08-01 15:19:22 +0800

    ks test fail,but,continue find for method !

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -4,16 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
-import java.util.Map;
 
 @Controller
 public class TestController {
 
-    static final String KSAPI = "http://api.ksapisrv.com/rest/n/photo/info2?kpf=IPHONE&net=%E4%B8%AD%E5%9B%BD%E7%A7%BB%E5%8A%A8_5&appver=6.6.2.1004&kpn=KUAISHOU&mod=iPhone8%2C1&c=a&ud=214451601&did_gt=1551354412209&ver=6.6&sys=ios12.3.1&did=39896A34-183F-44F6-A9D3-29553C501FE3&isp=CMCC";
 
 
     @GetMapping(value = {"/test", "9527"})
@@ -32,18 +26,5 @@ public class TestController {
         return "index";
     }
 
-    @GetMapping("/kstest")
-    @ResponseBody
-    public String ksSig() {
-        Map<String, String> stringStringMap = KuaiShouDecodeMain.urlDecode(KSAPI);
-        System.out.println(stringStringMap);
-        List<String> strings = KuaiShouDecodeMain.sigAlgorithm(stringStringMap, "5193776284807906285");
-        StringBuffer sb = new StringBuffer();
-        for (String s : strings) {
-            sb.append(s);
-        }
-        String clock = CPU.INSTANCE.getClock(null,sb.toString().getBytes(), 21);
-        System.out.println(clock);
-        return clock;
-    }
+
 }
