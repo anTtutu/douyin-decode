b82c6aa29e6629b2292ff801e755fb247ba7ca3c
亥冬 <755964539@qq.com>
2019-09-16 20:23:05 +0800

    1.support long and short links

--- src/main/java/com/example/demo/KSController.java
+++ src/main/java/com/example/demo/KSController.java
@@ -1,31 +1,31 @@
-//package com.example.demo;
-//
-//
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//import java.util.Map;
-//
-//@RestController
-//@RequestMapping("/ks")
-//public class KSController {
-//    static final String KSAPI = "http://api.ksapisrv.com/rest/n/photo/info2?kpf=IPHONE&net=%E4%B8%AD%E5%9B%BD%E7%A7%BB%E5%8A%A8_5&appver=6.6.2.1004&kpn=KUAISHOU&mod=iPhone8%2C1&c=a&ud=214451601&did_gt=1551354412209&ver=6.6&sys=ios12.3.1&did=39896A34-183F-44F6-A9D3-29553C501FE3&isp=CMCC";
-//
-//    @GetMapping("/kstest")
-//    @ResponseBody
-//    public String ksSig() {
-//        Map<String, String> stringStringMap = KuaiShouDecodeMain.urlDecode(KSAPI);
-//        System.out.println(stringStringMap);
-//        List<String> strings = KuaiShouDecodeMain.sigAlgorithm(stringStringMap, "5193776284807906285");
-//        StringBuffer sb = new StringBuffer();
-//        for (String s : strings) {
-//            sb.append(s);
-//        }
-//        String clock = CPU.getClock(null, sb.toString().getBytes(), 21);
-//        System.out.println(clock);
-//        return clock;
-//    }
-//}
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/ks")
+public class KSController {
+    static final String KSAPI = "http://api.ksapisrv.com/rest/n/photo/info2?kpf=IPHONE&net=%E4%B8%AD%E5%9B%BD%E7%A7%BB%E5%8A%A8_5&appver=6.6.2.1004&kpn=KUAISHOU&mod=iPhone8%2C1&c=a&ud=214451601&did_gt=1551354412209&ver=6.6&sys=ios12.3.1&did=39896A34-183F-44F6-A9D3-29553C501FE3&isp=CMCC";
+
+    @GetMapping("/kstest")
+    @ResponseBody
+    public String ksSig() {
+        Map<String, String> stringStringMap = KuaiShouDecodeMain.urlDecode(KSAPI);
+        System.out.println(stringStringMap);
+        List<String> strings = KuaiShouDecodeMain.sigAlgorithm(stringStringMap, "5193776284807906285");
+        StringBuffer sb = new StringBuffer();
+        for (String s : strings) {
+            sb.append(s);
+        }
+        String clock = CPU.getClock(null, sb.toString().getBytes(), 21);
+        System.out.println(clock);
+        return clock;
+    }
+}
--- src/main/java/com/example/demo/LibLoader.java
+++ src/main/java/com/example/demo/LibLoader.java
@@ -1,29 +1,29 @@
-//package com.example.demo;
-//
-//import org.apache.commons.io.FileUtils;
-//
-//import java.io.File;
-//import java.io.InputStream;
-//
-//public class LibLoader {
-//    public static void loadLib(String libName) {
-//        String resourcePath = "/" + libName;
-//        String folderName = System.getProperty("java.io.tmpdir") + "/lib/";
-//        File folder = new File(folderName);
-//        folder.mkdirs();
-//        File libFile = new File(folder, libName);
-//        if (libFile.exists()) {
-//            System.load(libFile.getAbsolutePath());
-//        } else {
-//            try {
-//                InputStream in = LibLoader.class.getResourceAsStream(resourcePath);
-//                FileUtils.copyInputStreamToFile(in,libFile);
-//                in.close();
-//                System.load(libFile.getAbsolutePath());
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//                throw new RuntimeException("Failed to load required lib", e);
-//            }
-//        }
-//    }
-//}
+package com.example.demo;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.InputStream;
+
+public class LibLoader {
+    public static void loadLib(String libName) {
+        String resourcePath = "/" + libName;
+        String folderName = System.getProperty("java.io.tmpdir") + "/lib/";
+        File folder = new File(folderName);
+        folder.mkdirs();
+        File libFile = new File(folder, libName);
+        if (libFile.exists()) {
+            System.load(libFile.getAbsolutePath());
+        } else {
+            try {
+                InputStream in = LibLoader.class.getResourceAsStream(resourcePath);
+                FileUtils.copyInputStreamToFile(in,libFile);
+                in.close();
+                System.load(libFile.getAbsolutePath());
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException("Failed to load required lib", e);
+            }
+        }
+    }
+}
